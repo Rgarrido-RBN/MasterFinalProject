f0fbf52047d319a8f7e11f99d6d86963bd767a49
ruben <rgarrido.rbn@gmail.com>
2021-06-06 17:41:35 +0200

    Fixing encoder report bugs

--- arduinoShieldMain/arduinoShieldMain.ino
+++ arduinoShieldMain/arduinoShieldMain.ino
@@ -1,18 +1,30 @@
 #include <SoftwareSerial.h>
+#include <TimerThree.h>
 
+#define COUNTS_PER_PULSE 4
+#define PULSES_PER_LAP 36
+#define DEBUG_PIN 2
 #define ENCODER_CHANNEL_A 20
 #define ENCODER_CHANNEL_B 21
-#define BUFFERSIZE 20
+#define PERIOD_MS 500.0
+#define MILLISEC_TO_MICROSEC 1000.0
+#define MILLISEC_TO_SEC 1/1000
+#define MILLISEC_TO_MIN 1/1000/60
 
 SoftwareSerial RPISerial(19, 18); // RX, TX 
 
 volatile unsigned long encoderPulse = 0;
 volatile long movementDirection = 1;
+float wheelSpeed = 0;
+float prevWheelSpeed = 0;
 
 void setup() 
 {
-  timerInit();
-  
+  pinMode(DEBUG_PIN, OUTPUT);
+
+  Timer3.initialize(PERIOD_MS * MILLISEC_TO_MICROSEC);
+  Timer3.attachInterrupt(encoderReport);
+
   Serial.begin(9600);
   RPISerial.begin(115200);
 
@@ -22,11 +34,5 @@ void setup()
 
 void loop()
 {
-  RPISerial.print("Pulses: ");RPISerial.print(encoderPulse);RPISerial.print("\n");
-  RPISerial.print("Direction: ");RPISerial.print(movementDirection);RPISerial.print("\n");
-  
-  Serial.print("Pulses: ");Serial.print(encoderPulse);Serial.print("\n");
-  Serial.print("Direction: ");Serial.print(movementDirection);Serial.print("\n");
-
   delay(10);
 }
--- arduinoShieldMain/encoderReports.ino
+++ arduinoShieldMain/encoderReports.ino
@@ -1,25 +1,19 @@
-void timerInit()
+void encoderReport()
 {
-  cli();
-  TCCR3A = 0;
-  TCCR3B = 0;
-  TCNT3  = 0;
-  OCR3A = 15624;
-  TCCR3B |= (1 << WGM12);
-  TCCR3B |= (1 << CS12) | (1 << CS10);  
-  TIMSK3 |= (1 << OCIE1A);  
-  sei();
-}
-ISR(TIMER3_COMPA_vect)
-{
-  long speed = encoderPulse/2400.00*60;
-  Serial.println(direction*speed);
-   encoderPulse = 0; 
+  // wheelSpeed = (encoderPulse / COUNTS_PER_PULSE / PULSES_PER_LAP) * (PERIOD_MS / MILLISEC_TO_MIN); //ENCODER RPM
+  wheelSpeed = (encoderPulse / COUNTS_PER_PULSE) * (PERIOD_MS); //PULSES PER MILLISEC
+  if(prevWheelSpeed != wheelSpeed)
+  {
+    RPISerial.print("RPM of Right encoder: ");RPISerial.print(wheelSpeed * movementDirection);RPISerial.print("\n");
+  }
+  prevWheelSpeed = wheelSpeed;
+  encoderPulse = 0; 
 }
 void encoderISRChannelA()
 {
   encoderPulse++;
   movementDirection = digitalRead(ENCODER_CHANNEL_A) == digitalRead(ENCODER_CHANNEL_B) ? -1 : 1;
+  digitalWrite(DEBUG_PIN, !digitalRead(DEBUG_PIN));
 }
 
 void encoderISRChannelB()
