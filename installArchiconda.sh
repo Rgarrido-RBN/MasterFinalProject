d3c324ae6f474e1f935799cf671e156b3505e046
Ubuntu <ubuntu@ubuntu.lan>
2021-09-12 16:30:53 +0000

    TFlite files added

---
+++ installArchiconda.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd ${HOME}
+wget https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh
+sudo sh Archiconda3-0.2.3-Linux-aarch64.sh
+rm -rf Archiconda3-0.2.3-Linux-aarch64.sh
+cd ~
+sudo chown -R $USER archiconda3/
+export "PATH=/bin:/usr/bin:$PATH" >> ~/.bashrc 
+source ~/.bashrc
+conda config --add channels conda-forge
+conda -V
+
+
